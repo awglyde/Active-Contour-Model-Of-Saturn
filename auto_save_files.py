5a81e276a5c0c5ae9036c82399876a9562703e4d
tmduddy <tmd44@pitt.edu>
2016-09-29 22:23:49 -0400

    added comments to denoise function
    also added comments to path and file

--- auto_save_files.py
+++ auto_save_files.py
@@ -3,15 +3,20 @@ from scipy import misc, ndimage
 import numpy as np
 import os
 
+
 def denoise(im):
+    '''
+    Input: takes in image data array
+    Returns: image data array, with gaussian denoising applied
+    '''
     noisy = im + 0.4 * im.std() * np.random.random(im.shape)
     return ndimage.gaussian_filter(noisy,2)
 
-path = "C:/Users/tmd44/Desktop/images/"
-save = "C:/Users/tmd44/Desktop/denoise/"
+
+path = "C:/Users/tmd44/Desktop/images/" #location of FITS files to take in
+save = "C:/Users/tmd44/Desktop/denoise/" #location to save denoised images
 
 for file in os.listdir(path):
     autograb_data = fits.open(path + file)
     image_data = autograb_data[0].data
     misc.imsave(save + file + '.jpg', denoise(image_data))
-    
