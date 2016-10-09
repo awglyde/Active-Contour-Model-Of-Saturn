2d610e9dcc3ea70a3a803ecbf1bb2a7dffdfb1ac
Alex Glyde <awglyde@gmail.com>
2016-10-09 14:04:26 -0400

    modifying script to only get fits files

--- auto_save_files.py
+++ auto_save_files.py
@@ -7,10 +7,12 @@ def denoise(im):
     noisy = im + 0.4 * im.std() * np.random.random(im.shape)
     return ndimage.gaussian_filter(noisy,2)
 
-path = "C:/Users/tmd44/Desktop/images/"
-save = "C:/Users/tmd44/Desktop/denoise/"
+path = "C:/Github/ASTRON-1263/Saturn/0.01/"
+save = "C:/Github/ASTRON-1263/Saturn/denoise/"
 
 for file in os.listdir(path):
-    autograb_data = fits.open(path + file)
-    image_data = autograb_data[0].data
-    misc.imsave(save + file + '.jpg', denoise(image_data))
+    print(file)
+    if file.endswith(".fits"):
+        autograb_data = fits.open(path + file)
+        image_data = autograb_data[0].data
+        misc.imsave(save + file + '.jpg', denoise(image_data))
