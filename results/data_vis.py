e58363415731d528c91506262975908233dc98a5
tmduddy <tmd44@pitt.edu>
2016-11-28 13:07:22 -0500

    Add subplots for three histograms

--- results/data_vis.py
+++ results/data_vis.py
@@ -1,20 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.clf()
+
 data1 = np.genfromtxt("/Users/tyler/Desktop/astro files/final_proj_stat.csv", 
     delimiter=',')
 top_ten = len(data1)/10
 top_five= len(data1)/20
-data2 = data1[:top_ten]
-data3 = data1[:top_five]
+data2 = np.sort(data1)[:top_ten]
+data3 = np.sort(data1)[:top_five]
 
-plt.hist(data1,1200, label="All 1101 Images")
+bins = np.arange(0,700,35)
 
-#plt.hist(data2, 5, label="Top 10%", alpha=0.5)
-
+plt.subplot(3,1,1)
+plt.hist(data1,bins=bins, label="All 1101 Images")
+plt.title("Distribution of Euclidean Dist. from the Good Snake to each Test Snake")
+plt.ylabel('Number of Images')
 plt.xlim(0,700)
-plt.title("Euclidean Distances from the Good Snake to each Test Snake")
+plt.ylim(0,210)
+plt.legend(loc='upper right')
+plt.grid(True)
+
+plt.subplot(3,1,2)
+#plt.hist(data2, bins=np.arange(0,200,5), label="Closest 10%", color='green')
+plt.hist(data2, bins=bins, label="Closest 10%", color='green')
+plt.legend(loc='upper right')
+plt.ylabel('Number of Images')
+plt.grid(True)
+
+plt.subplot(3,1,3)
+plt.hist(data1,bins=bins, label="All 1101 Images")
+plt.hist(data2, bins=bins, label="Closest 10%", alpha=1)
 plt.xlabel('Euclidean Distances [pixels]')
 plt.ylabel('Number of Images')
+plt.ylim(0,210)
 plt.legend(loc='upper right')
+plt.grid(True)
+
+
 plt.show()
